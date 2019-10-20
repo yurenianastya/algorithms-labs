d95bbd78c8e57bc71eb0640e19eb0b6bb9a07c80
Nsty <nastyayurenia@gmail.com>
2019-10-20 23:39:51 +0300

    changing script file

--- lab2/script.py
+++ lab2/script.py
@@ -13,15 +13,17 @@ class Farm:
         for element in range(0, boxes):
             stables.append(int(input()))
         stables.sort()
-        return print("Available spaces: ", stables)
+        print("Available spaces: ", stables)
+        return stables
+
 
     def average_len(self, stables, crazy_cows):
         stables_length = stables[-1] - stables[0]
         average_length = math.trunc(stables_length / crazy_cows)
-        return print("The expected average length is: ", average_length)
+        print("The expected average length is: ", average_length)
+        return average_length
 
     def iterating_list(self, stables, average_length, crazy_cows):
-        center = math.floor(len(stables)/2)
         crazy_cows_stable = [stables[0], stables[-1]]
         crazy_cows = crazy_cows - 2
         right_flag = -1
@@ -29,7 +31,10 @@ class Farm:
         left_flag = 0
         left_flag1 = 1
         while crazy_cows > 0:
-            for j in range(0, center-1, 1):  # left side loop
+            for j in range(0, len(stables)-1, 1):  # left side loop
+                if stables[left_flag1] in crazy_cows_stable:
+                    left_flag1 = left_flag1 + 1
+                    break
                 if stables[left_flag1] - stables[left_flag] >= average_length:
                     crazy_cows_stable.append(stables[left_flag1])
                     left_flag = left_flag1
@@ -38,7 +43,10 @@ class Farm:
                     break
                 else:
                     left_flag1 = left_flag1 + 1
-            for k in range(len(stables), center, -1):  # right side loop
+            for k in range(len(stables), 0, -1):  # right side loop
+                if stables[left_flag1] in crazy_cows_stable:
+                    right_flag1 = right_flag1 + 1
+                    break
                 if stables[right_flag] - stables[right_flag1] >= average_length:
                     crazy_cows_stable.append(stables[right_flag1])
                     right_flag = right_flag1
@@ -47,14 +55,25 @@ class Farm:
                     break
                 else:
                     right_flag1 = right_flag1 - 1
-        return print("Answer: ", crazy_cows_stable)
+        print("The places where crazy cows are: ", sorted(crazy_cows_stable))
+        return crazy_cows_stable
+
+    def lowest_distance(self, crazy_cows_stable):
+        add_array = []
+        crazy_cows_stable.sort()
+        for i in range(0, len(crazy_cows_stable)-1):
+            number = crazy_cows_stable[i+1]-crazy_cows_stable[i]
+            add_array.append(number)
+        print("The smallest distance is: ", min(add_array))
+        return min(add_array)
 
 
-obj = Farm(17, 4)
-obj.stables = [1, 3, 5, 10, 15, 16, 18, 20, 22, 24, 28, 33, 35, 39, 41, 49, 51]
-obj.average_length = 12
-print(len(obj.stables))
-print(obj.stables[-1])
-obj.iterating_list(obj.stables, obj.average_length, obj.crazy_cows)
+# def the objects
+the_farm = Farm(8, 5)
+stables1 = the_farm.generate_stables(the_farm.boxes, the_farm.crazy_cows)
+the_len = the_farm.average_len(stables1, the_farm.crazy_cows)
+the_iter = the_farm.iterating_list(stables1, the_len, the_farm.crazy_cows)
+the_distance = the_farm.lowest_distance(the_iter)
+
 
 
