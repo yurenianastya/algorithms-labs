c57e2129d9882b86f4a0cfdc2f5a4f7dd7a0ed42
Nsty <nastyayurenia@gmail.com>
2019-10-21 01:55:28 +0300

    Adding tests and fixing code

--- lab2/script.py
+++ lab2/script.py
@@ -16,16 +16,20 @@ class Farm:
         print("Available spaces: ", stables)
         return stables
 
-
-    def average_len(self, stables, crazy_cows):
+    def average_len(self, stables, crazy_cows, boxes):
         stables_length = stables[-1] - stables[0]
         average_length = math.trunc(stables_length / crazy_cows)
-        print("The expected average length is: ", average_length)
+        # if crazy_cows >= int(boxes/2):
+        #     average_length = average_length - 2
         return average_length
 
     def iterating_list(self, stables, average_length, crazy_cows):
         crazy_cows_stable = [stables[0], stables[-1]]
         crazy_cows = crazy_cows - 2
+        if crazy_cows == 1:
+            center = math.trunc(len(stables)/2)-1
+            crazy_cows_stable.append(stables[center])
+            crazy_cows = crazy_cows - 1
         right_flag = -1
         right_flag1 = -2
         left_flag = 0
@@ -35,7 +39,7 @@ class Farm:
                 if stables[left_flag1] in crazy_cows_stable:
                     left_flag1 = left_flag1 + 1
                     break
-                if stables[left_flag1] - stables[left_flag] >= average_length:
+                elif stables[left_flag1] - stables[left_flag] >= average_length:
                     crazy_cows_stable.append(stables[left_flag1])
                     left_flag = left_flag1
                     left_flag1 = left_flag1 + 1
@@ -47,7 +51,7 @@ class Farm:
                 if stables[left_flag1] in crazy_cows_stable:
                     right_flag1 = right_flag1 + 1
                     break
-                if stables[right_flag] - stables[right_flag1] >= average_length:
+                elif stables[right_flag] - stables[right_flag1] >= average_length:
                     crazy_cows_stable.append(stables[right_flag1])
                     right_flag = right_flag1
                     right_flag1 = right_flag1 - 1
@@ -67,13 +71,6 @@ class Farm:
         print("The smallest distance is: ", min(add_array))
         return min(add_array)
 
-
-# def the objects
-the_farm = Farm(8, 5)
-stables1 = the_farm.generate_stables(the_farm.boxes, the_farm.crazy_cows)
-the_len = the_farm.average_len(stables1, the_farm.crazy_cows)
-the_iter = the_farm.iterating_list(stables1, the_len, the_farm.crazy_cows)
-the_distance = the_farm.lowest_distance(the_iter)
 
 
 
--- lab2/test_farm.py
+++ lab2/test_farm.py
@@ -1,12 +1,41 @@
 from unittest import TestCase
+import script
 
 
 class TestFarm(TestCase):
-    def test_generate_stables(self):
-        self.fail()
 
-    def test_average_len(self):
-        self.fail()
+    def test_obj_1(self):
+        print("----------Testing the case when 3 cows----------")
+        object1 = script.Farm(8, 3)
+        stables = [1, 5, 6, 7, 8, 9, 10, 11]
+        the_len = object1.average_len(stables, object1.crazy_cows, object1.boxes)
+        the_iter = object1.iterating_list(stables, the_len, object1.crazy_cows)
+        the_distance = object1.lowest_distance(the_iter)
+        self.assertEqual(the_distance, 4)
 
-    def test_iterating_list(self):
-        self.fail()
+    def test_obj_2(self):
+        print("----------Testing the case when cows equal the half of the boxes----------")
+        object2 = script.Farm(12, 6)
+        stables = [1, 5, 6, 7, 8, 9, 10, 11, 14, 17, 19, 22]
+        the_len = object2.average_len(stables, object2.crazy_cows, object2.boxes)
+        the_iter = object2.iterating_list(stables, the_len, object2.crazy_cows)
+        the_distance = object2.lowest_distance(the_iter)
+        self.assertEqual(the_distance, 3)
+
+    def test_obj_3(self):
+        print("----------Testing the case when many cows----------")
+        object3 = script.Farm(24, 7)
+        stables = [1, 5, 6, 7, 8, 9, 10, 11, 14, 18, 20, 21, 27, 30, 33, 34, 37, 40, 42, 48, 50, 55, 56, 63]
+        the_len = object3.average_len(stables, object3.crazy_cows, object3.boxes)
+        the_iter = object3.iterating_list(stables, the_len, object3.crazy_cows)
+        the_distance = object3.lowest_distance(the_iter)
+        self.assertEqual(the_distance, 7)
+
+    def test_obj_4(self):
+        print("----------Testing the random case----------")
+        object4 = script.Farm(18, 8)
+        stables = [1, 5, 8, 9, 10, 11, 25, 29, 34, 37, 40, 49, 50, 55, 60, 63, 70]
+        the_len = object4.average_len(stables, object4.crazy_cows, object4.boxes)
+        the_iter = object4.iterating_list(stables, the_len, object4.crazy_cows)
+        the_distance = object4.lowest_distance(the_iter)
+        self.assertEqual(the_distance, 6)
